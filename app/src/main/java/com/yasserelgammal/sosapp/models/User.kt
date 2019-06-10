effee8df1147220bdb549bb510828f85ddd5a94f
Yasser Elgammal <35766609+YasserElgammal@users.noreply.github.com>
2019-06-10 19:54:18 +0200

    Delete User.kt

--- app/src/main/java/com/yasserelgammal/sosapp/models/User.kt
+++
@@ -1,8 +0,0 @@
-package com.yasserelgammal.sosapp.models
-
-data class User(val id:Int,
-                val email:String,
-                val name:String,
-                val phone:String,
-                val sosName:String,
-                val sosPhone:String)
